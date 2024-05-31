bfa9ff5e938d8d11b3d628cccd67ee1b3f3ff14e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-31 14:54:01 +0100

    Add click event listner to save button

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 const currentDateEL = $("#currentDay");
+const saveBtnEl = $(".saveBtn");
+const textareaEl = $(".description");
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,6 +11,17 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  saveBtnEl.on("click", (event) => {
+    event.preventDefault();
+
+    const parentEl = $(event.target).closest(".time-block");
+    const plandId = parentEl.attr("id");
+    const planDescription = textareaEl.val();
+
+    console.log(parentEl);
+  });
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
